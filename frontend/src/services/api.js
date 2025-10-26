ce49d74ac2ce37f7335938aa6ccaf4c6eac575e6
Rex-8 <rehaanjosemathew@gmail.com>
2025-10-26 19:45:00 +0530

    ft:removed-old-frontend

--- frontend/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({ baseURL: "/api" });
-
-export const registerUser = (data) => API.post("/auth/register", data);
-export const loginUser = (data) => API.post("/auth/login", data);
-export const getTracks = () => API.get("/tracks");
